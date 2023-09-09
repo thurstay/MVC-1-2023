e0a454a46cac24718d9395ea75a369aa98c0bf46
thurstay <87205484+thurstay@users.noreply.github.com>
2023-09-09 12:33:51 +0700

    Delete View.java

--- View.java
+++
@@ -1,9 +0,0 @@
-public class View {
-    public void printModel1(Model1 model1) {
-        System.out.println(model1.getValue() + " is " + model1.getType());
-    }
-
-    public void printModel2(Model2 model2) {
-        System.out.println(model2.getValue() + " is " + model2.getType());
-    }
-}
